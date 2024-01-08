3ce8022ee733442527f554e720169714a16dd597
maciejstanek <eszpag@gmail.com>
2024-01-08 12:02:25 +0100

    Implemented assignment 1

--- 01/main.cxx
+++ 01/main.cxx
@@ -1,6 +1,31 @@
+#include <array>
 #include <iostream>
+#include <string>
+#include <thread>
 
 auto main() -> int {
-    std::cout << "Hello, Maciej!\n";
+    auto const limit = 100;
+    std::array<std::string_view, 4> const names{
+        "Abdul", "Bart", "Claudia", "Divya"};
+    auto const fizzbuzzer = [limit, &names]() {
+        for (auto i = 1; i <= limit; ++i) {
+            std::string value;
+            if (i % 3 == 0) {
+                value += "fizz";
+            }
+            if (i % 5 == 0) {
+                value += "buzz";
+            }
+            if (value.empty()) {
+                value += std::to_string(i);
+            } else {
+                value += "!";
+            }
+            std::cout << names[(i - 1) % names.size()]
+                      << " says " << value << '\n';
+        }
+    };
+    std::thread thr(fizzbuzzer);
+    thr.join();
     return 0;
 }
