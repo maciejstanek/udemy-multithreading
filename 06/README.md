32acd757c94f1f8e669a5fd444e39938fe14b2b0
maciejstanek <eszpag@gmail.com>
2024-01-10 09:08:26 +0100

    Completed assignment #6

---
+++ 06/README.md
@@ -0,0 +1,10 @@
+Example result:
+
+```
+starting
+printer boots up
+adder computes the result for "6" and "7"
+printer awaits
+adder sets the promised value
+printer got "13"
+```
--- 06/main.cxx
+++ 06/main.cxx
@@ -1,16 +1,31 @@
 #include <chrono>
 #include <future>
 #include <iostream>
+#include <thread>
 
 auto main() -> int {
-    std::cout << "Starting" << std::endl;
-    std::packaged_task<int(int, int)> ptask([](int a, int b) {
+    auto const adder = [](std::promise<int>& prom, int a, int b) {
+        std::cout << "adder computes the result for \"" << a << "\" and \"" << b
+                  << "\"" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "adder sets the promised value" << std::endl;
+        prom.set_value(a + b);
+    };
+    auto const printer = [](std::future<int>& fut) {
+        std::cout << "printer boots up" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        return a + b;
-    });
-    auto fut = ptask.get_future();
-    std::thread thr(std::move(ptask), 6, 7);
-    std::cout << fut.get() << std::endl;
-    thr.join();
+        std::cout << "printer awaits" << std::endl;
+        auto value = fut.get();
+        std::cout << "printer got \"" << value << "\"" << std::endl;
+
+    };
+
+    std::cout << "starting" << std::endl;
+    std::promise<int> prom;
+    auto fut = prom.get_future();
+    auto const a = 6;
+    auto const b = 7;
+    std::jthread thr1(adder, std::ref(prom), a, b);
+    std::jthread thr2(printer, std::ref(fut));
     return 0;
 }
